41d65feb9ff1fbd4f97d3489755087849cc4dbfe
Mateusz Kaliński <mateusz.kalinski@student.pk.edu.pl>
2025-02-05 16:57:55 +0100

    Add files via upload

--- kapitanStatku.cpp
+++ kapitanStatku.cpp
@@ -1,38 +1,76 @@
 #include "common.h"
 #include <cstring>
 
-// flaga sygnalu1 (wczesniejsze wyplyniecie)
+// -----------------------------
+// Globalne zmienne i flagi
+// -----------------------------
+
+// Ta flaga ustawia sie na 1, gdy otrzymamy sygnal SIGUSR1 (wczesniejszy rejs)
 static volatile sig_atomic_t earlyDeparture = 0;
 
-static int semid = -1; // identyfikator semaforow
-static int shmid = -1; // identyfikator pamieci dzielonej
+// Ta flaga ustawia sie, gdy otrzymamy SIGINT (np. Ctrl+C)
+static volatile sig_atomic_t ctrlC = 0;
+
+// Identyfikatory zasobow systemowych (semaforow, pamieci dzielonej)
+static int semid = -1;
+static int shmid = -1;
+
+// Wskaznik do danych w pamieci dzielonej
 static SharedData* shdata = nullptr;
 
-// Sygnal1 => wczesniejsze wyplyniecie
+// Zabezpieczenie, zeby nie usuwac zasobow kilka razy
+static bool resourcesCleaned = false;
+
+
+// -----------------------------
+// Handlery sygnalow
+// -----------------------------
+
+// Sygnal1 (SIGUSR1) => wczesniejsze wyplyniecie (jesli trwa zaladunek)
 void sigusr1_handler(int)
 {
     earlyDeparture = 1;
 }
 
-// Sygnal2 => endOfDay
+// Sygnal2 (SIGUSR2) => endOfDay=true (konczymy rejsy)
 void sigusr2_handler(int)
 {
-    // Ustawiamy w pamieci dzielonej flage endOfDay
     if(shdata) {
         shdata->endOfDay = true;
     }
 }
 
-// forceUnload => wyladowanie pasazerow natychmiast
-// ustawia SEM_DIR=0 i zwieksza semafory, by nikt nie zostal na statku/mostku
+// SIGINT (np. Ctrl+C) => ustaw endOfDay i ctrlC
+void sigint_handler(int)
+{
+    colorLog("[KapitanStatku] Otrzymalem SIGINT, ustawiam endOfDay.", COL_RED);
+    if(shdata) {
+        shdata->endOfDay = true;
+    }
+    ctrlC = 1;
+}
+
+
+// -----------------------------
+// Funkcje pomocnicze
+// -----------------------------
+
+// forceUnload()
+// Natychmiast wyrzuca wszystkich pasazerow ze statku i mostka
+// (zwieksza semafory, aby staly sie maksymalne)
 void forceUnload()
 {
     colorLog("[KapitanStatku] forceUnload: wymuszam wyladowanie pasazerow!", COL_BRED);
+    if(!shdata) return; // jesli cos jest nie tak z pamiecia, nic nie robimy
+
+    // Wylaczamy zaladunek, wlaczamy wyladunek
     shdata->loading = false;
     shdata->disembarking = true;
+
+    // SEM_DIR=0 oznacza wykladanie
     setSemValue(semid, SEM_DIR, 0);
 
-    // Zwiekszamy SEM_SHIP dopoki nie bedzie N (pusty statek)
+    // Zwiekszamy SEM_SHIP dopoki (SEM_SHIP == N), co znaczy pusty statek
     while(true) {
         int freeOnShip = getSemValue(semid, SEM_SHIP);
         if(freeOnShip == shdata->N) {
@@ -41,7 +79,8 @@ void forceUnload()
         semOp(semid, SEM_SHIP, +1);
         usleep(100000);
     }
-    // Zwiekszamy SEM_BRIDGE dopoki nie bedzie K (pusty mostek)
+
+    // Zwiekszamy SEM_BRIDGE dopoki (SEM_BRIDGE == K), co oznacza pusty mostek
     while(true) {
         int freeOnBridge = getSemValue(semid, SEM_BRIDGE);
         if(freeOnBridge == shdata->K) {
@@ -50,12 +89,49 @@ void forceUnload()
         semOp(semid, SEM_BRIDGE, +1);
         usleep(100000);
     }
+
     colorLog("[KapitanStatku] forceUnload: wyladowanie zakonczone.", COL_BRED);
 }
 
+// cleanupResources()
+// Usuwa semafory i pamiec dzielona tylko raz (dzieki resourcesCleaned)
+void cleanupResources()
+{
+    if(!resourcesCleaned) {
+        resourcesCleaned = true; 
+        colorLog("[KapitanStatku] Czyszcze zasoby IPC (sem, shm)...", COL_GRAY);
+
+        // Odlaczamy sie od pamieci dzielonej
+        if(shdata) {
+            detachShm(shdata);
+            shdata = nullptr;
+        }
+
+        // Usuwamy pamiec dzielona (shmctl IPC_RMID)
+        if(shmid != -1) {
+            if(shmctl(shmid, IPC_RMID, nullptr)==-1) {
+                perror("shmctl IPC_RMID");
+            }
+            shmid = -1;
+        }
+
+        // Usuwamy semafory (semctl IPC_RMID)
+        if(semid != -1) {
+            if(semctl(semid, 0, IPC_RMID)==-1) {
+                perror("semctl IPC_RMID");
+            }
+            semid = -1;
+        }
+    }
+}
+
+
+// -----------------------------
+// MAIN - Glowna funkcja
+// -----------------------------
 int main(int argc, char* argv[])
 {
-    // Odczyt argumentow: N, K, R, T2
+    // Sprawdzamy czy podano 4 parametry: N, K, R, T2
     if(argc < 5) {
         std::cerr << "Uzycie: " << argv[0] << " <N> <K> <R> <T2>\n";
         exit(1);
@@ -65,7 +141,7 @@ int main(int argc, char* argv[])
     int R_ = atoi(argv[3]);
     int T2_ = atoi(argv[4]);
 
-    // Walidacja parametrow
+    // Prosta walidacja parametrow
     if(N_<=0 || K_<=0 || R_<=0 || T2_<=0) {
         std::cerr << "Parametry musza byc > 0!\n";
         exit(1);
@@ -75,86 +151,99 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    // Tworzenie kluczy IPC
+    // Rejestrujemy obsluge sygnalow SIGUSR1, SIGUSR2, SIGINT
+    {
+        struct sigaction sa1, sa2, sa3;
+        memset(&sa1, 0, sizeof(sa1));
+        sa1.sa_handler = sigusr1_handler;
+        sigaction(SIGUSR1, &sa1, nullptr);
+
+        memset(&sa2, 0, sizeof(sa2));
+        sa2.sa_handler = sigusr2_handler;
+        sigaction(SIGUSR2, &sa2, nullptr);
+
+        memset(&sa3, 0, sizeof(sa3));
+        sa3.sa_handler = sigint_handler;
+        sigaction(SIGINT, &sa3, nullptr);
+    }
+
+    // Tworzymy klucze do semaforow i pamieci
     key_t keySem = ftok(FTOK_PATH, 0x11);
     if(keySem == -1) { perror("ftok sem"); exit(1); }
     key_t keyShm = ftok(FTOK_PATH, 0x12);
     if(keyShm == -1) { perror("ftok shm"); exit(1); }
 
-    // Tworzymy semafory: SEM_COUNT = 3
+    // Tworzymy semafory z flaga IPC_CREAT
     semid = semget(keySem, SEM_COUNT, 0600 | IPC_CREAT);
     if(semid == -1) {
         perror("semget");
         exit(1);
     }
-    // Ustaw wartosci: mostek=K_, statek=N_, SEM_DIR=1 (zaladunek)
+    // Ustawiamy startowe wartosci semaforow
     setSemValue(semid, SEM_BRIDGE, K_);
     setSemValue(semid, SEM_SHIP,   N_);
     setSemValue(semid, SEM_DIR,    1);
 
-    // Tworzymy pamiec dzielona
+    // Tworzymy/pobieramy pamiec dzielona
     shmid = shmget(keyShm, sizeof(SharedData), 0600 | IPC_CREAT);
     if(shmid == -1) {
         perror("shmget");
+        // Jesli nie udalo sie utworzyc pamieci, sprzatamy semafory
+        cleanupResources();
         exit(1);
     }
-    // Dolaczamy sie do pamieci
     shdata = attachShm(shmid);
 
-    // Inicjalizujemy dane w pamieci dzielonej
+    // Nadpisujemy dane w pamieci dzielonej - na wypadek, gdyby cos starego tam bylo
     shdata->kapitanStatkuPID = getpid();
-    shdata->generatorPID = 0; // wypelni pozniej generator
+    shdata->generatorPID = 0;      // ustawi to generator
     shdata->endOfDay = false;
     shdata->traveling = false;
     shdata->loading = true;
     shdata->disembarking = false;
     shdata->rejsCount = 0;
-
-    // Zapisujemy parametry
     shdata->N = N_;
     shdata->K = K_;
     shdata->R = R_;
     shdata->T2= T2_;
 
-    // Ustawiamy obsluge sygnalow SIGUSR1, SIGUSR2
-    struct sigaction sa1, sa2;
-    memset(&sa1,0,sizeof(sa1));
-    sa1.sa_handler = sigusr1_handler;
-    if(sigaction(SIGUSR1,&sa1,nullptr)==-1) {
-        perror("sigaction1");
-    }
-    memset(&sa2,0,sizeof(sa2));
-    sa2.sa_handler = sigusr2_handler;
-    if(sigaction(SIGUSR2,&sa2,nullptr)==-1) {
-        perror("sigaction2");
-    }
-
-    // Log startu
+    // Log o starcie
     {
         std::string msg = "[KapitanStatku] start. (K="+std::to_string(K_)+", N="+std::to_string(N_)+")";
         colorLog(msg, COL_BGREEN);
     }
     colorLog("[KapitanStatku] Zaladunek: czekam, az statek sie wypelni lub sygnal1. Jesli sygnal2 -> end.", COL_WHITE);
 
-    // Glowna petla rejsow
+    // Czekamy, az generator (pasazer.cpp) uruchomi sie i ustawi generatorPID
+    while(!shdata->endOfDay && !ctrlC && shdata->generatorPID == 0) {
+        usleep(100000);
+    }
+
+    // Glowna petla rejsow:
     while(true) {
-        // Jesli osiagnieto R rejsow lub endOfDay==true, konczymy
-        if(shdata->rejsCount >= shdata->R || shdata->endOfDay) {
+        // Jesli przekroczono liczbe R, ustaw endOfDay
+        if(shdata->rejsCount >= shdata->R) {
+            shdata->endOfDay = true;
+            break;
+        }
+        // Jesli sygnal2 lub Ctrl+C, wychodzimy
+        if(shdata->endOfDay || ctrlC) {
             break;
         }
 
+        // Start zaladunku
         earlyDeparture=0;
-        shdata->loading=true;
-        shdata->disembarking=false;
-        setSemValue(semid, SEM_DIR, 1); // 1=zaladunek
+        shdata->loading = true;
+        shdata->disembarking = false;
+        setSemValue(semid, SEM_DIR, 1);
 
-        // Czekamy az statek sie zapelni LUB wczesniejsze wyplyniecie LUB endOfDay
+        // Czekamy az statek sie zapelni LUB sygnal1 LUB sygnal2
         while(true) {
-            if(shdata->endOfDay) {
+            if(shdata->endOfDay || ctrlC) {
                 colorLog("[KapitanStatku] Sygnal2 w trakcie zaladunku => forceUnload i koniec.", COL_RED);
                 forceUnload();
-                shdata->endOfDay = true; // zaznaczamy koniec
-                goto KONIEC;
+                shdata->endOfDay = true;
+                goto FINISH;
             }
             if(earlyDeparture) {
                 colorLog("[KapitanStatku] Sygnal1 => wczesniejsze wyplyniecie (koncze zaladunek).", COL_CYAN);
@@ -167,32 +256,35 @@ int main(int argc, char* argv[])
             }
             usleep(200000);
         }
-        // Koniec zaladunku
-        shdata->loading=false;
-        if(shdata->endOfDay) {
+
+        // Zaladunek konczy sie
+        shdata->loading = false;
+        if(shdata->endOfDay || ctrlC) {
             break;
         }
 
+        // Ile pasazerow jest na statku
         int passengersOnShip = N_ - getSemValue(semid, SEM_SHIP);
         {
             std::string msg = "[KapitanStatku] Koniec zaladunku. Na statku jest "+std::to_string(passengersOnShip)+" pasazerow.";
             colorLog(msg, COL_WHITE);
         }
 
-        // Czekamy az mostek pusty
+        // Czekamy, az mostek bedzie pusty (SEM_BRIDGE==K)
         while(true) {
-            if(shdata->endOfDay) {
+            if(shdata->endOfDay || ctrlC) {
                 colorLog("[KapitanStatku] Sygnal2 w trakcie czekania => forceUnload i koniec.", COL_RED);
                 forceUnload();
-                goto KONIEC;
+                shdata->endOfDay = true;
+                goto FINISH;
             }
             int valB = getSemValue(semid, SEM_BRIDGE);
-            if(valB==K_) break;
+            if(valB == K_) break;
             usleep(100000);
         }
 
         // Wyplywamy w rejs
-        shdata->traveling=true;
+        shdata->traveling = true;
         {
             std::string msg = "[KapitanStatku] Wyplywam w rejs nr "+std::to_string(shdata->rejsCount+1)+
                               ", T2="+std::to_string(shdata->T2)+"s";
@@ -200,61 +292,57 @@ int main(int argc, char* argv[])
         }
         sleep(shdata->T2);
 
-        shdata->traveling=false;
+        // Koniec rejsu
+        shdata->traveling = false;
         shdata->rejsCount++;
         {
             std::string msg = "[KapitanStatku] Wrocilem z rejsu nr "+std::to_string(shdata->rejsCount);
             colorLog(msg, COL_BBLUE);
         }
 
-        if(shdata->endOfDay) {
+        // Jesli sygnal2 po rejsie, to wyrzucamy pasazerow i konczymy
+        if(shdata->endOfDay || ctrlC) {
             colorLog("[KapitanStatku] Sygnal2 w trakcie rejsu => dokonczyc i wyladowanie, koniec.", COL_RED);
             forceUnload();
+            shdata->endOfDay = true;
             break;
         }
 
         // Wyladunek
-        shdata->disembarking=true;
+        shdata->disembarking = true;
         setSemValue(semid, SEM_DIR, 0);
         colorLog("[KapitanStatku] Rozpoczynam wyladunek po rejsie...", COL_WHITE);
 
-        // Czekamy az statek bedzie pusty (SEM_SHIP==N)
         while(true) {
             int freeOnShip = getSemValue(semid, SEM_SHIP);
-            if(freeOnShip == N_) break;
-            if(shdata->endOfDay) {
+            if(freeOnShip == N_) break; 
+            if(shdata->endOfDay || ctrlC) {
                 colorLog("[KapitanStatku] Sygnal2 w trakcie wyladunku => forceUnload i koniec.", COL_RED);
                 forceUnload();
-                goto KONIEC;
+                shdata->endOfDay = true;
+                goto FINISH;
             }
             usleep(200000);
         }
-        shdata->disembarking=false;
-        colorLog("[KapitanStatku] Zakonczylem wyladunek.", COL_WHITE);
 
-        if(shdata->rejsCount >= shdata->R) {
-            std::string msg = "[KapitanStatku] Osiagnieto R="+std::to_string(shdata->R)+". Koniec dnia.";
-            colorLog(msg, COL_BRED);
-            shdata->endOfDay=true;
-            break;
-        }
-        colorLog("[KapitanStatku] Zaladunek: czekam, az statek sie wypelni lub sygnal1. Jesli sygnal2 -> end.", COL_WHITE);
+        shdata->disembarking = false;
+        colorLog("[KapitanStatku] Zakonczylem wyladunek.", COL_WHITE);
     }
 
-KONIEC:
+// Etykieta FINISH - konczymy program
+FINISH:
     {
         std::string msg = "[KapitanStatku] Konczy. rejsCount="+std::to_string(shdata->rejsCount)+
                           " endOfDay="+std::to_string(shdata->endOfDay);
         colorLog(msg, COL_BRED);
     }
 
-    // Czekamy, az generator sie zakonczy (kill(generatorPID, 0) => ESRCH)
+    // Jesli mamy generatorPID, czekamy az generator sie zakonczy (kill(...0) => ESRCH = brak procesu)
     if(shdata->generatorPID > 0) {
         std::string msg = "[KapitanStatku] Czekam, az generator (pid="+std::to_string(shdata->generatorPID)+") sie zakonczy...";
         colorLog(msg, COL_GRAY);
 
         while(true) {
-            // kill(pid,0) zwraca -1 z ESRCH, jesli procesu nie ma
             if(kill(shdata->generatorPID, 0)==-1 && errno==ESRCH) {
                 break;
             }
@@ -262,14 +350,8 @@ KONIEC:
         }
     }
 
-    // Usuwamy semafory i pamiec
-    detachShm(shdata);
-    if(shmctl(shmid, IPC_RMID, nullptr)==-1) {
-        perror("shmctl IPC_RMID");
-    }
-    if(semctl(semid, 0, IPC_RMID)==-1) {
-        perror("semctl IPC_RMID");
-    }
+    // Na samym koncu usuwamy semafory i pamiec (jesli jeszcze nie byly usuniete)
+    cleanupResources();
 
     return 0;
 }
